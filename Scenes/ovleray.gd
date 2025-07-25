7eddcd239a412435a38193dc5f1440219080d4f5
Colin Jacobs <colin@coljac.net>
2025-07-25 14:09:29 +1000

    overlay mouse event

--- Scenes/ovleray.gd
+++ Scenes/ovleray.gd
@@ -1,7 +1,7 @@
 extends Control
-# var x_pixel: float = 0.0
-# var cursor_line_color: Color = Color(1, 0, 0, 1)
-# var cursor_line_width: float = 1.0
+
+func _ready():
+	mouse_filter = Control.MOUSE_FILTER_IGNORE
 
 func _draw() -> void:
 	var x_pixel = get_parent()._microns_to_pixels(get_parent().cursor_wavelength)
